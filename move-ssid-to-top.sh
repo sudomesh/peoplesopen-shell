ff4d02048b60a5a84930af9385b5790560cf08bf
wwwhtml <danarauz@gmail.com>
2018-05-27 14:17:08 -0700

    started working on it, still ot fully functional

--- move-ssid-to-top.sh
+++ move-ssid-to-top.sh
@@ -9,19 +9,33 @@
 #Description: This script moves an SSID to the top of the list.
 
 clear
+WiFiDevice=""
+SSIDvar=""
+SSIDSecurityTypevar=""
+SSIDPasswordvar=""
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
-echo "| TITLE GOES HERE:                                                            |"
+echo "| Moving SSID to the top of the List:                                         |"
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
+networksetup -listallhardwareports
+echo " "
+read -p "Input the WiFi Device (Ex: en0 or en1 or etc), then press [ENTER]: " WiFiDevice 
+clear
+echo "Current Preferred Wireless Networks:"
+echo "+- - - - - - - - - - -+"
+networksetup -listpreferredwirelessnetworks "$WiFiDevice"
 echo
-
-
-
-
-
-
-
-
+read -p "Input the WiFi SSID to MOVE to the top of the list, then press [ENTER]: " SSIDvar
+read -p "Input the WiFi SSID Security Type (ex: WPA2 or None or etc), then press [ENTER]: " SSIDSecurityTypevar
+read -p "Input computer admin password, then press [ENTER]: " SSIDPasswordvar
+clear
+echo "+- - - - - - - - - - -+"
+# networksetup -addpreferredwirelessnetworkatindex <device name> <network> <index> <security type> [password]
+networksetup -addpreferredwirelessnetworkatindex $WiFiDevice $SSIDvar 0 $SSIDSecurityTypevar "$SSIDPasswordvar"
+clear
+echo "+- - - - - - - - - - -+"
+networksetup -listpreferredwirelessnetworks $WiFiDevice
 echo
+echo "Process completed."
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
 read -p "Press enter to continue"
