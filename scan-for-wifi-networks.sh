def0799f8bed80334ed957b71ed6e46f8466c288
wwwhtml <danarauz@gmail.com>
2018-06-08 22:05:53 -0700

    adding hostname and date

--- scan-for-wifi-networks.sh
+++ scan-for-wifi-networks.sh
@@ -7,8 +7,11 @@
 #Date: May 26, 2018
 
 #Description: This script scans for wifi networks.
-
 clear
+echo "Computer Hostname: " 
+hostname
+date
+echo ""
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
 echo "| Please wait a moment, scanning for available WiFi Networks...               |"
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
@@ -16,7 +19,9 @@ echo
 cd /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/
 AIRPORTTRIGGER="./airport -s"
 $AIRPORTTRIGGER
+date
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
 echo "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+"
+
 read -p "Press enter to continue"
 clear
